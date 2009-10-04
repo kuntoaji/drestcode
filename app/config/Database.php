49e01cb5ed286410947a34ebb1b68c4f4173ce8b
kunto aji <aji_okay@yahoo.co.id>
2009-10-04 17:45:11 +0000

    Adding documentation and license

--- app/config/Database.php
+++ app/config/Database.php
@@ -1,5 +1,29 @@
 <?php
+/**
+    Database abstract class. Database configuration file.
+    Copyright (C) 2009  Kunto Aji Kristianto
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 abstract class Database {
+	/**
+	 * Database Configuration
+	 *
+	 * @var string $database_host, $database_name, $database_user, $database_password
+	 * @access protected $database_host, $database_name, $database_user, $database_password
+	 */
 	protected $database_host = 'localhost';
 	protected $database_name = 'db_name';
 	protected $database_user = 'root';
--- app/config/Service.php
+++ app/config/Service.php
@@ -1,5 +1,30 @@
 <?php
+/**
+    Service class. Define name of controller class that will be active.
+    Right now, Drestcode can only run one controller at the same time.
+    Copyright (C) 2009  Kunto Aji Kristianto
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 class Service {
+	/**
+	 * Name of active controller
+	 *
+	 * @var string
+	 * @access public static
+	 */
 	public static $active_controller = 'YourController';
 }
 ?>
